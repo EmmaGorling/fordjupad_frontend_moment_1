227963a7134bb10f3e030fc952403352c7944e0f
Emma Ådahl Görling <143135641+EmmaGorling@users.noreply.github.com>
2025-01-28 17:03:22 +0100

    Lagt till komponent för tv serier, med array med serier samt utskrift av dessa

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import { useState } from 'react';
 import './App.css'
 import Header from './components/Header';
 import Footer from './components/Footer';
+import TvShows from './components/TvShows';
 
 function App() {
   const [title, setTitle] = useState("Min serie-lista")
   return (
     <>
       <Header siteTitle={title} />
-
+      <main>
+        <TvShows />
+      </main>
       <Footer siteTitle={title} />
     </>
   )
---
+++ src/components/TvShows.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import './TvShows.css'
+
+interface Show {
+    title: string;
+    seen: boolean;
+    seasons: number;
+}
+
+const TvShows = () => {
+    
+    const shows: Show[] = [
+        { title: "The Walking Dead", seen: true, seasons: 11},
+        { title: "Yellowstone", seen: true, seasons: 5 },
+        { title: "Arcane", seen: true, seasons: 2 },
+        { title: "Bridgerton", seen: false, seasons: 3 },
+        { title: "Big Mouth", seen: true, seasons: 7 },
+        { title: "Outlander", seen: false, seasons: 7 },
+        { title: "Lost", seen: true, seasons: 6 }, 
+        { title: "The Office", seen: false, seasons: 9 }
+    ]
+    return (
+        <ul>
+            {
+                shows.map((show, index) => (
+                    <li key={index}>
+                        <h2>{show.title}</h2>
+                        <p className='seasons'><span className='semibold'>Antal säsonger:</span> {show.seasons}</p>
+                        <p className='seen'><span className='semibold'>Har sett:</span> {show.seen ? (
+                            <i className="fa-solid fa-check"></i>
+                        ) : ('inte än')}
+                        </p>
+                    </li>
+                ))
+            }
+        </ul>
+    )
+}
+
+export default TvShows
